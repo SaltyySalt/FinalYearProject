1d3cec0414cd5e7010de98432b257856645286bb
Fatin <156884188+SaltyySalt@users.noreply.github.com>
2025-05-28 10:13:13 +0800

    Update server

---
+++ nfc-registration-site/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  uid: { type: String, unique: true, required: true },
+  name: String,
+  matric: String,
+  phone: String,
+  photo: String
+});
+
+module.exports = mongoose.model('User', userSchema);
